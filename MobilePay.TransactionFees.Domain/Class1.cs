fd52d14b1e35559e582d7364b6137d73ef9687ad
tomasbingelis <bingelis.tomas@gmail.com>
2018-12-09 12:53:44 +0200

    Initial commit

---
+++ MobilePay.TransactionFees.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MobilePay.TransactionFees.Domain
+{
+    public class Class1
+    {
+    }
+}
---
+++ MobilePay.TransactionFees.Program/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MobilePay.TransactionFees.Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
